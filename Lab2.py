174ee40100ea86dbca0a7ba741d71a05cf19f3b3
Bryan <bryankong.22@ichat.sp.edu.sg>
2023-05-02 14:54:49 +0800

    Lab2.py

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,28 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def display_main_menu():
+    print("Enter some numbers separated by commas(e.g. 5, 67, 32)")
+
+def get_user_input():
+    print('Enter values:')
+    x = input()
+    y = x.split(", ")
+    temp = []
+    for item in y:
+        temp.append(float(item))
+    return temp
+
+def calc_average_temperature(y):
+    avg = sum(y) / len(y)
+    print(avg)
+    return avg
+
+def calc_min_max_temperature(z):
+    minimum = min(z)
+    maximum = max(z)
+    print(maximum, minimum)
+    return minimum, maximum
+
+display_main_menu()
+global temp
+temp = get_user_input()
+calc_average_temperature(temp)
+calc_min_max_temperature(temp)
